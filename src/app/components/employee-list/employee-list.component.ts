7b37f0bfef03e93d82f302b865329a8f3d6e8ca9
Jannik Kastens <jannik.kastens@hmmh.de>
2024-01-24 08:52:13 +0100

    Add employee details link

--- src/app/components/employee-list/employee-list.component.ts
+++ src/app/components/employee-list/employee-list.component.ts
@@ -1,26 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Observable, of } from "rxjs";
-import { Employee } from "../../entitys/Employee";
+import { Observable, of } from 'rxjs';
+import { Employee } from '../../entitys/Employee';
 import { MatIconModule } from '@angular/material/icon';
 import { EmployeeEntry } from '../employee-entry/employee-entry.component';
 import { EmployeeService } from '../../services/employee.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-list',
   standalone: true,
-  imports: [
-    CommonModule,
-    MatIconModule,
-    EmployeeEntry
-  ],
+  imports: [CommonModule, MatIconModule, EmployeeEntry],
   templateUrl: './employee-list.component.html',
-  styleUrl: './employee-list.component.css'
+  styleUrl: './employee-list.component.css',
 })
 export class EmployeeListComponent implements OnInit {
   employees$: Observable<Employee[]>;
 
-  constructor(private service: EmployeeService) {
+  constructor(
+    private service: EmployeeService,
+    private router: Router,
+  ) {
     this.employees$ = of([]);
   }
 
@@ -31,5 +31,9 @@ export class EmployeeListComponent implements OnInit {
 
   fetchData() {
     this.employees$ = this.service.getAllEmployees();
+  }
+
+  goToDetails(employee: Employee) {
+    this.router.navigate(['/employees', employee.id]);
   }
 }
