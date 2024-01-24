8d4fe8b94c75880ee336d8dea0122b0f6e9741c2
Jannik Kastens <jannik.kastens@hmmh.de>
2024-01-24 12:32:27 +0100

    Add employee-details input fields and switch

--- src/app/components/employee-details/employee-details.component.ts
+++ src/app/components/employee-details/employee-details.component.ts
@@ -5,17 +5,20 @@ import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-employee-details',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, FormsModule, CommonModule],
   templateUrl: './employee-details.component.html',
   styleUrl: './employee-details.component.css',
 })
 export class EmployeeDetailsComponent {
   destroy$ = new Subject();
-  selectedEmployee: Employee | null = null;
+  selectedEmployee: Employee = new Employee();
+  editing = false;
 
   constructor(
     private employeeService: EmployeeService,
@@ -25,7 +28,13 @@ export class EmployeeDetailsComponent {
   ngOnInit() {
     this.employeeService.selectedEmployee$
       .pipe(takeUntil(this.destroy$))
-      .subscribe((employee) => (this.selectedEmployee = employee));
+      .subscribe((employee) => {
+        if (employee == null) {
+          return;
+        }
+
+        this.selectedEmployee = employee;
+      });
   }
 
   ngOnDestroy() {
@@ -36,5 +45,9 @@ export class EmployeeDetailsComponent {
 
   navigateBack() {
     this.router.navigate(['/employees']);
+  }
+
+  toggleEdit() {
+    this.editing = !this.editing;
   }
 }
