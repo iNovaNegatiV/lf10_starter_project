0e8f1ece8faac0e9689946c1d6b23f69582e6f23
critcap <critcap.dev@gmail.com>
2024-01-28 22:32:55 +0100

    DROP

--- src/app/components/employee-list/employee-list.component.ts
+++ src/app/components/employee-list/employee-list.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Observable, of } from "rxjs";
 import { Employee } from "../../entitys/Employee";
 import { MatIconModule } from '@angular/material/icon';
 import { EmployeeEntry } from '../employee-entry/employee-entry.component';
 import { EmployeeService } from '../../services/employee.service';
+import { MatDialog } from '@angular/material/dialog'; // Import MatDialog
+import { YesNoPromptComponent } from '../shared/yes-no-prompt/yes-no-prompt.component';
 
 @Component({
   selector: 'app-employee-list',
@@ -17,10 +19,11 @@ import { EmployeeService } from '../../services/employee.service';
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.css'
 })
+@Inject('YesNoPromptComponent')
 export class EmployeeListComponent implements OnInit {
   employees$: Observable<Employee[]>;
 
-  constructor(private service: EmployeeService) {
+  constructor(private service: EmployeeService, public dialog: MatDialog) {
     this.employees$ = of([]);
   }
 
@@ -31,5 +34,15 @@ export class EmployeeListComponent implements OnInit {
 
   fetchData() {
     this.employees$ = this.service.getAllEmployees();
+  }
+
+  openDialog(): void { // Create a method that opens the dialog
+    const dialogRef = this.dialog.open(YesNoPromptComponent, {
+      data: { question: 'Do you want to delete this employee?' }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 }
