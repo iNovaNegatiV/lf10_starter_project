a2aa74da3bdfe9b763dc3bb9fadda7c9315f6138
Jannik Kastens <jannik.kastens@hmmh.de>
2024-01-30 15:02:08 +0100

    Create employee-skill-details component

---
+++ src/app/components/employee-skill-details/employee-skill-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-skill-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './employee-skill-details.component.html',
+  styleUrl: './employee-skill-details.component.css'
+})
+export class EmployeeSkillDetailsComponent {
+
+}
